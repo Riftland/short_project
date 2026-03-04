ef275b3e0d0a5f0cae1183aed95ffe7bbd8d108d
Fco. Molpeceres <fran.mscr@gmail.com>
2026-03-04 20:09:13 +0100

    chore: added run migrations script

---
+++ scripts/migrate-up.js
@@ -0,0 +1,48 @@
+const fs = require("fs/promises");
+const path = require("path");
+const db = require("../src/config/db");
+const { sql } = require("slonik");
+
+const migrationsPath = path.join(__dirname, "../migrations");
+
+const createControl = sql.unsafe`
+  CREATE TABLE IF NOT EXISTS migrations (
+    id VARCHAR(50) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+  );
+`;
+
+const selectMigratedIds = sql.unsafe`
+  SELECT id FROM migrations;
+`;
+
+const updateControl = (id) => sql.unsafe`
+  INSERT INTO migrations (id)
+  VALUES (${id});
+`;
+
+(async () => {
+  try {
+    const migrationsDir = await fs.readdir(migrationsPath);
+    if (!migrationsDir.length) throw Error("No migration files found");
+
+    const connection = await db;
+
+    await connection.query(createControl);
+    const migratedIds = await connection.query(selectMigratedIds);
+    const migratedList = new Set(migratedIds.rows.map(({ id }) => id));
+
+    for await (const file of migrationsDir) {
+      if (migratedList.has(file)) {
+        console.log(`> file ${file} migrated, skipping...`);
+        continue;
+      }
+      const { up } = require(path.join(migrationsPath, file));
+      await connection.query(up);
+      await connection.query(updateControl(file));
+    }
+    console.log("> [SUCCESS]: migrations done! 🚀");
+  } catch (error) {
+    console.log("> [ERROR]:", error.message);
+  }
+})();
