d1acbac6cb665118206fbb945eecfeea5d360b06
Fco. Molpeceres <fran.mscr@gmail.com>
2026-03-11 21:32:48 +0100

    chore: created api url constant

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -1,7 +1,9 @@
 const API_PORT = process.env.API_PORT;
 const DB_URL = process.env.DB_URL;
+const API_URL = process.env.API_URL;
 
 module.exports = {
   API_PORT,
   DB_URL,
+  API_URL: `${API_URL}:${API_PORT}`,
 };
