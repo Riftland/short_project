71ae737f3fe39afa7f3a1c12e6d4aab5e05893ed
Fco. Molpeceres <fran.mscr@gmail.com>
2026-04-08 20:00:31 +0200

    chore: set PORT as env var instead

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -1,4 +1,4 @@
-const API_PORT = process.env.API_PORT;
+const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
 const API_URL = process.env.API_URL;
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -14,9 +14,9 @@ const DataFields = {
 };
 
 module.exports = {
-  API_PORT,
+  PORT,
   DB_URL,
-  API_URL: `${API_URL}:${API_PORT}`,
+  API_URL: `${API_URL}:${PORT}`,
   JWT_SECRET,
   DEFAULT_COOKIE_NAME,
   DEFAULT_EXP_TIME,
