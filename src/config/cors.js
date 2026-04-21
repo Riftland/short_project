c1c2e2835c2f6454d5fcfffed546ed3bfb5d343e
Fco. Molpeceres <fran.mscr@gmail.com>
2026-04-21 22:02:08 +0200

    fix: resolved import

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,13 +1,12 @@
-const errors = require("../misc/errors");
 const WHITELIST = ["localhost"];
 
 module.exports = {
   origin: (origin, callback) => {
+    console.log("> data:", { origin, callback });
     if (!origin) return callback(null, true);
-
     const match = WHITELIST.find((domain) => origin.includes(domain));
 
-    match ? callback(null, true) : callback(errors["cors"]);
+    match ? callback(null, true) : callback(null, false);
   },
   credentials: true,
 };
